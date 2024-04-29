d2819cd099c13b763d45a29495f71c8bf19e998f
gustavoLadchuk <gusta.ladchuk@gmail.com>
2024-04-29 19:49:20 -0300

    Exercícios do TDE 04

---
+++ TDE 04/ex01.js
@@ -0,0 +1,18 @@
+const callbackSucesso = (valor) =>
+{
+    console.log("A operação foi concluída com sucesso, o numero somado é par: " + valor)
+}
+
+const callbackError = (valor) =>
+{
+    console.log("A operação falhou, o numero somado é ímpar: " + valor)
+}
+
+function soma(numero1, numero2, sucesso, erro)
+{
+    resultado = numero1 + numero2
+    if (resultado % 2 == 0) sucesso(resultado)
+    else erro(resultado)
+}
+
+soma(2, 5, callbackSucesso, callbackError)
---
+++ TDE 04/ex02.js
@@ -0,0 +1,17 @@
+function soma(numero1, numero2)
+{
+   return new Promise((resolve, reject) =>
+{
+    resultado = numero1 + numero2
+
+    if (resultado % 2 == 0) 
+    resolve("A operação foi concluída com sucesso, o numero somado é par: "+ resultado)
+    else 
+    reject("A operação falhou, o numero somado é ímpar: " + resultado)
+})
+}
+
+soma(2, 3)
+.then(resultado => {console.log(resultado)})
+.catch(erro => {console.log(erro)})
+
